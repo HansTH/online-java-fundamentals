f09e332eebe4548447139540e888c52a8dd4f312
HansTH <hbhorst@mac.com>
2024-04-30 23:09:49 +0200

    complete exercises.

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,30 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
+        // write your code below
+        byte b = 2;
+        System.out.println("byte b is: " + b);
+
+        short s = 3;
+        System.out.println("short s is: " + s);
+
         int i = 1;
         System.out.println("int i is: " + i);
 
-        // write your code below
+        long l = 4L;
+        System.out.println("long l is: " + l);
+
+        float f = 5.0f;
+        System.out.println("float f is: " + f);
+
+        double d = 6.0;
+        System.out.println("double d is: " + d);
+
+        char c = 'A';
+        System.out.println("char c is: " + c);
+
+        boolean bool = true;
+        System.out.println("boolean bool is: " + bool);
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,34 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int length = str.length();
+        System.out.println(length);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean isEquals = str.equals(str2);
+        System.out.println(isEquals);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String str3 = str + str2;
+        System.out.println(str3);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        String replace = str.replace("hello", "world");
+        System.out.println(replace);
+
+        String sub = str.substring(0, 2);
+        System.out.println(sub);
+
+        boolean con = str.contains("hello");
+        System.out.println(con);
+
+        String concat = str.concat("world");
+        System.out.println(concat);
+
+        int index = str.indexOf("o");
+        System.out.println(index);
 
     }
 
